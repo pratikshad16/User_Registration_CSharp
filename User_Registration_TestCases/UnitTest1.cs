2e01562636e4c7da77824d71baa43d176be293ec
pratikshad16 <dhagepratiksha16@gmail.com>
2020-09-21 22:51:52 +0530

    Added: Welcome Message

---
+++ User_Registration_TestCases/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace User_Registration_TestCases
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
