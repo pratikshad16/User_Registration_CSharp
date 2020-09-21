529159d4caa6e373399838a4a43af38cf8e4b430
pratikshad16 <dhagepratiksha16@gmail.com>
2020-09-22 01:38:09 +0530

    Added: Test cases for validating a password with one special chracter

--- User_Registration_TestCases/UnitTest1.cs
+++ User_Registration_TestCases/UnitTest1.cs
@@ -101,12 +101,19 @@ namespace User_Registration_TestCases
             bool result = userRegistration.ValidatePasswordPattern("Pratbghfd");
             Assert.IsFalse(result);
         }
-            [Test]
-            public void GivenPassword_WhenContainMoreNumericNumber_ShouldReturnTrue()
-            {
-                UserRegistrationMain userRegistration = new UserRegistrationMain();
-                bool result = userRegistration.ValidatePasswordPattern("Pra@tb5h7d");
-                Assert.IsTrue(result);
-            }
+        [Test]
+        public void GivenPassword_WhenContainMoreNumericNumber_ShouldReturnTrue()
+        { 
+           UserRegistrationMain userRegistration = new UserRegistrationMain();
+           bool result = userRegistration.ValidatePasswordPattern("Pra@tb5h7d");
+           Assert.IsTrue(result);
         }
+        [Test]
+        public void GivenPassword_WhenNotContainSpecialChar_ShouldReturnFalse()
+        {
+            UserRegistrationMain userRegistration = new UserRegistrationMain();
+            bool result = userRegistration.ValidatePasswordPattern("Pratb5h7d");
+            Assert.IsFalse(result);
+        }
+    }
 }
