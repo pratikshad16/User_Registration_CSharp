13c5e33d1bf14ea4cd645d0b6781b32bccb06ccc
pratikshad16 <dhagepratiksha16@gmail.com>
2020-09-21 23:28:27 +0530

    Added: Code and test cases for validating first name

---
+++ User_Registration/UserRegistrationMain.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace User_Registration
+{
+    public class UserRegistrationMain
+    {
+        string FirstName = "^[A-Z][a-z]{2,}$";
+        public bool ValidateFirstName(string firstName)
+        {
+            return Regex.IsMatch(firstName, FirstName);
+        }
+    }
+}
--- User_Registration_TestCases/UnitTest1.cs
+++ User_Registration_TestCases/UnitTest1.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using User_Registration;
 
 namespace User_Registration_TestCases
 {
@@ -10,9 +11,18 @@ namespace User_Registration_TestCases
         }
 
         [Test]
-        public void Test1()
+        public void GivenFirstName_WhenStartsWithCapAndMinThreeChar_ShouldReturnTrue()
         {
-            Assert.Pass();
+            UserRegistrationMain userRegistration = new UserRegistrationMain();
+            bool result = userRegistration.ValidateFirstName("Pratiksha");
+            Assert.IsTrue(result);
+        }
+        [Test]
+        public void GivenFirstName_WhenStartsWithCapAndLessThanThreeChar_ShouldReturnTrue()
+        {
+            UserRegistrationMain userRegistration = new UserRegistrationMain();
+            bool result = userRegistration.ValidateFirstName("Pr");
+            Assert.IsFalse(result);
         }
     }
 }
